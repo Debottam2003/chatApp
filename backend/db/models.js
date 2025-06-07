e66e077dbd61bb4e36139aad753048149b4fe6f7
Debottam Kar <debottamkar2003@gmail.com>
2025-06-07 17:30:12 +0530

    message model

--- backend/db/models.js
+++ backend/db/models.js
@@ -21,7 +21,28 @@ let userSchema = new mongoose.Schema({
 
 export const users = mongoose.model('users', userSchema);
 
+let messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    text: {
+        type: String,
+    },
+    image: {
+        type: String
+    }
+},
+{timestamps: true}
+);
 
+export const messages = mongoose.model('messages', messageSchema);
 
 
 
